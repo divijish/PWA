410697d3a91fef0d7cd7cda46a090c28e43ede13
Divij Shrivastava <divij@Divijs-MacBook-Pro.local>
2020-08-21 12:48:33 +0530

    Added logos

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+
+
 window.onload = ()=>{
-    console.log("Script is getting loaded!")
+    
+    if('serviceWorker' in navigator){
+        try{
+        navigator.serviceWorker.register('../sw.js');
+        console.log("Service Worker registered");
+        }catch(e){
+            console.log(`Could not register service worker :${e}`);
+        }
+    }
+
 }
+
+window.addEventListener('appinstalled', event =>{
+    console.log("App Installed", event);
+})
